9f1a9beb8d146b1885af78deae52cce204934552
jorman901 <jormanjairr@gmail.com>
2026-08-31 12:04:57 -0500

    Punteros

---
+++ Punteros.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Punteros {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = 75;
+        System.out.println("Valor de N " + n);
+        System.out.println("Direccion " + n);
+    }
+}
