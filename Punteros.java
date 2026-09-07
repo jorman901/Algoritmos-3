4165ded1e531f800637643cf0e671973962d0055
jorman901 <jormanjairr@gmail.com>
2026-09-07 14:47:17 -0500

    Cpp

--- Punteros.java
+++ Punteros.java
@@ -1,9 +1,24 @@
 import java.util.Scanner;
 public class Punteros {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        int n = 75;
-        System.out.println("Valor de N " + n);
-        System.out.println("Direccion " + n);
+        Scanner scanner = new Scanner(System.in);
+        int dato = 0;
+        int[] numeros = new int[5];
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Ingrese numero entero "+(i + 1)+ ": "  );
+            dato = scanner.nextInt();
+            numeros[i] = dato;
+        }
+        int[] puntero = numeros; // puntero apunta al mismo arreglo que numeros
+        System.out.println("Contenido del arreglo original:");
+        for (int num : numeros) {
+            System.out.print(num + " ");
+        }
+        // Modificando el arreglo a través del puntero
+        puntero[0] = 10;
+        System.out.println("\nContenido del arreglo después de modificar a traves del puntero:");
+        for (int num : numeros) {
+            System.out.print(num + " ");
+        }
     }
 }
