bfe21a736b146a1e1e523da5dafe2e0f77cf5592
jorman901 <jormanjairr@gmail.com>
2026-09-14 12:39:46 -0500

    Polinomios

--- Codigo.cpp
+++ Codigo.cpp
@@ -4,107 +4,134 @@
 #include <string>
 
 using namespace std;
-struct Termino {
+struct Termino
+{
     float coeficiente;
-    int   exponente;
-    Termino* siguiente;
+    int exponente;
+    Termino *siguiente;
 };
-Termino* crearNodo(float coef, int exp) {
-    Termino* nuevo = new Termino();
+Termino *crearNodo(float coef, int exp)
+{
+    Termino *nuevo = new Termino();
     nuevo->coeficiente = coef;
-    nuevo->exponente   = exp;
-    nuevo->siguiente   = nullptr;
+    nuevo->exponente = exp;
+    nuevo->siguiente = nullptr;
     return nuevo;
 }
-Termino* insertarTermino(Termino* cabeza, float coef, int exp) {
-    if (coef == 0.0f) return cabeza;
-    if (cabeza == nullptr || exp > cabeza->exponente) {
-        Termino* nuevo   = crearNodo(coef, exp);
+Termino *insertarTermino(Termino *cabeza, float coef, int exp)
+{
+    if (coef == 0.0f)
+        return cabeza;
+    if (cabeza == nullptr || exp > cabeza->exponente)
+    {
+        Termino *nuevo = crearNodo(coef, exp);
         nuevo->siguiente = cabeza;
         return nuevo;
-    }   
-   if (cabeza->exponente == exp) {
+    }
+    if (cabeza->exponente == exp)
+    {
         cabeza->coeficiente += coef;
-        if (cabeza->coeficiente == 0.0f) {
-            Termino* temp = cabeza->siguiente;
+        if (cabeza->coeficiente == 0.0f)
+        {
+            Termino *temp = cabeza->siguiente;
             delete cabeza;
             return temp;
         }
         return cabeza;
     }
-    Termino* actual = cabeza;
+    Termino *actual = cabeza;
     while (actual->siguiente != nullptr &&
-           actual->siguiente->exponente > exp) {
+           actual->siguiente->exponente > exp)
+    {
         actual = actual->siguiente;
     }
 
-     if (actual->siguiente != nullptr &&
-        actual->siguiente->exponente == exp) {
+    if (actual->siguiente != nullptr &&
+        actual->siguiente->exponente == exp)
+    {
         actual->siguiente->coeficiente += coef;
-        if (actual->siguiente->coeficiente == 0.0f) {
-            Termino* temp    = actual->siguiente->siguiente;
+        if (actual->siguiente->coeficiente == 0.0f)
+        {
+            Termino *temp = actual->siguiente->siguiente;
             delete actual->siguiente;
             actual->siguiente = temp;
         }
-    } else {
-         Termino* nuevo       = crearNodo(coef, exp);
-        nuevo->siguiente     = actual->siguiente;
-        actual->siguiente    = nuevo;
+    }
+    else
+    {
+        Termino *nuevo = crearNodo(coef, exp);
+        nuevo->siguiente = actual->siguiente;
+        actual->siguiente = nuevo;
     }
     return cabeza;
 }
-void imprimirPolinomio(Termino* cabeza) {
-    if (cabeza == nullptr) {
+void imprimirPolinomio(Termino *cabeza)
+{
+    if (cabeza == nullptr)
+    {
         cout << "0";
         return;
     }
-    Termino* actual  = cabeza;
-    bool     primero = true;
+    Termino *actual = cabeza;
+    bool primero = true;
 
-    while (actual != nullptr) {
+    while (actual != nullptr)
+    {
         float coef = actual->coeficiente;
-        int   exp  = actual->exponente;
+        int exp = actual->exponente;
 
-        if (primero) {
-            if (coef < 0) cout << "-";
+        if (primero)
+        {
+            if (coef < 0)
+                cout << "-";
             primero = false;
-        } else {
+        }
+        else
+        {
             cout << (coef < 0 ? " - " : " + ");
         }
 
         float absCoef = fabs(coef);
 
-        if (exp == 0) {
+        if (exp == 0)
+        {
             cout << absCoef;
-        } else if (absCoef != 1.0f) {
+        }
+        else if (absCoef != 1.0f)
+        {
             cout << absCoef;
         }
 
-
-        if (exp == 1)       cout << "x";
-        else if (exp > 1)   cout << "x^" << exp;
+        if (exp == 1)
+            cout << "x";
+        else if (exp > 1)
+            cout << "x^" << exp;
 
         actual = actual->siguiente;
     }
     cout << endl;
 }
 
-int destruirPolinomio(Termino*& cabeza) {
+int destruirPolinomio(Termino *&cabeza)
+{
     int contador = 0;
-    while (cabeza != nullptr) {
-        Termino* temp = cabeza;
-        cabeza        = cabeza->siguiente;
+    while (cabeza != nullptr)
+    {
+        Termino *temp = cabeza;
+        cabeza = cabeza->siguiente;
         delete temp;
         contador++;
     }
     return contador;
 }
-Termino* sumarPolinomios(Termino* p1, Termino* p2) {
-    Termino* resultado = nullptr;
+Termino *sumarPolinomios(Termino *p1, Termino *p2)
+{
+    Termino *resultado = nullptr;
 
     // Copiar todos los términos de P1
-    Termino* actual = p1;
-    while (actual != nullptr) {
+    Termino *actual = p1;
+    while (actual != nullptr)
+    {
         resultado = insertarTermino(resultado,
                                     actual->coeficiente,
                                     actual->exponente);
@@ -113,7 +140,8 @@ Termino* sumarPolinomios(Termino* p1, Termino* p2) {
 
     // Sumar (insertar) todos los términos de P2
     actual = p2;
-    while (actual != nullptr) {
+    while (actual != nullptr)
+    {
         resultado = insertarTermino(resultado,
                                     actual->coeficiente,
                                     actual->exponente);
@@ -123,15 +151,18 @@ Termino* sumarPolinomios(Termino* p1, Termino* p2) {
     return resultado;
 }
 
-Termino* multiplicarPolinomios(Termino* p1, Termino* p2) {
-    Termino* resultado = nullptr;
-    Termino* t1        = p1;
+Termino *multiplicarPolinomios(Termino *p1, Termino *p2)
+{
+    Termino *resultado = nullptr;
+    Termino *t1 = p1;
 
-    while (t1 != nullptr) {
-        Termino* t2 = p2;
-        while (t2 != nullptr) {
+    while (t1 != nullptr)
+    {
+        Termino *t2 = p2;
+        while (t2 != nullptr)
+        {
             float nuevoCoef = t1->coeficiente * t2->coeficiente;
-            int   nuevoExp  = t1->exponente   + t2->exponente;
+            int nuevoExp = t1->exponente + t2->exponente;
             resultado = insertarTermino(resultado, nuevoCoef, nuevoExp);
             t2 = t2->siguiente;
         }
@@ -140,25 +171,30 @@ Termino* multiplicarPolinomios(Termino* p1, Termino* p2) {
     return resultado;
 }
 
-float evaluarPolinomio(Termino* cabeza, float x) {
-    float    resultado = 0.0f;
-    Termino* actual    = cabeza;
+float evaluarPolinomio(Termino *cabeza, float x)
+{
+    float resultado = 0.0f;
+    Termino *actual = cabeza;
 
-    while (actual != nullptr) {
+    while (actual != nullptr)
+    {
         resultado += actual->coeficiente * pow(x, actual->exponente);
         actual = actual->siguiente;
     }
     return resultado;
 }
 
-Termino* derivarPolinomio(Termino* cabeza) {
-    Termino* resultado = nullptr;
-    Termino* actual    = cabeza;
+Termino *derivarPolinomio(Termino *cabeza)
+{
+    Termino *resultado = nullptr;
+    Termino *actual = cabeza;
 
-    while (actual != nullptr) {
-        if (actual->exponente != 0) {
+    while (actual != nullptr)
+    {
+        if (actual->exponente != 0)
+        {
             float nuevoCoef = actual->coeficiente * actual->exponente;
-            int   nuevoExp  = actual->exponente - 1;
+            int nuevoExp = actual->exponente - 1;
             resultado = insertarTermino(resultado, nuevoCoef, nuevoExp);
         }
         actual = actual->siguiente;
@@ -166,46 +202,60 @@ Termino* derivarPolinomio(Termino* cabeza) {
     return resultado;
 }
 
-int contarTerminos(Termino* cabeza) {
-    int      contador = 0;
-    Termino* actual   = cabeza;
-    while (actual != nullptr) {
+int contarTerminos(Termino *cabeza)
+{
+    int contador = 0;
+    Termino *actual = cabeza;
+    while (actual != nullptr)
+    {
         contador++;
         actual = actual->siguiente;
     }
     return contador;
 }
 
-int gradoPolinomio(Termino* cabeza) {
-    if (cabeza == nullptr) return -1;  // polinomio vacío
+int gradoPolinomio(Termino *cabeza)
+{
+    if (cabeza == nullptr)
+        return -1; // polinomio vacío
     return cabeza->exponente;
 }
 
-void separador(const string& titulo) {
-    cout << "\n" << string(55, '=') << endl;
+void separador(const string &titulo)
+{
+    cout << "\n"
+         << string(55, '=') << endl;
     cout << "  " << titulo << endl;
     cout << string(55, '=') << endl;
 }
-int main (){
-    Termino* nodo = nullptr;
-    Termino* nodo2 = nullptr;
-    float coeficiente=0;
-    int exponente=0;
-    nodo = insertarTermino (nodo, 4.0f,3);
-    nodo = insertarTermino (nodo, -2.0f,1);
-    nodo = insertarTermino (nodo, 5.0f,0);
-    nodo = insertarTermino (nodo, -1.0f,2);
+int main()
+{
+    Termino *nodo = nullptr;
+    Termino *nodo2 = nullptr;
+    float coeficiente = 0;
+    int exponente = 0;
+    /*
+    nodo = insertarTermino(nodo, 4.0f, 3);
+    nodo = insertarTermino(nodo, -2.0f, 1);
+    nodo = insertarTermino(nodo, 5.0f, 0);
+    nodo = insertarTermino(nodo, -1.0f, 2);
     cout << "Polinomio (x)";
     imprimirPolinomio(nodo);
-    cout << "Grado: " << gradoPolinomio(nodo) <<endl;
-    cout << "Nodos: " << contarTerminos(nodo) <<endl;
+    */
+    cout << "Grado: " << gradoPolinomio(nodo) << endl;
+    cout << "Nodos: " << contarTerminos(nodo) << endl;
     for (int i = 0; i < 4; i++)
     {
-        cout <<"Ingresa el coeficiente: ";
+        cout << "Ingresa el coeficiente: ";
         cin >> coeficiente;
-        cout <<"Ingresa el exponente: ";
+        cout << "Ingresa el exponente: ";
         cin >> exponente;
-        nodo2=insertarTermino(nodo2,coeficiente,exponente);
+        nodo2 = insertarTermino(nodo2, coeficiente, exponente);
     }
-    imprimirPolinomio(nodo2);    
+    imprimirPolinomio(nodo2);
+    cout << "Liberar memoria"<<endl;
+    int liberar = 0;
+    liberar = destruirPolinomio(nodo);
+    liberar = destruirPolinomio(nodo2);
+    cout << "Se libero el nodo 1 y nodo 2";
 }
